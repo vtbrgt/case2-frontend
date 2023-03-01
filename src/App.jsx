3a10573d30d73347c24ea8704c7a6ff626abd2b4
Gabriel Gervasio <gabrieldesantana2002@gmail.com>
2023-03-01 16:55:14 -0300

    Desatualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { Outlet } from 'react-router-dom'
+
 import { useState } from 'react'
 import './App.css'
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+import Rotas from './routes/Rotas.jsx'
+
+import NavbarReact from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
-      
+    <div className="App">
+      <NavbarReact />
+      <Rotas />
+      <div /*className='container'*/>
+        <Outlet />
+      </div>
+
     </div>
   )
 }
