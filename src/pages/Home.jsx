fe90cc7307077877fd8b57e3091c765cded7755d
Jean Vitor <114100988+jeanvitor29@users.noreply.github.com>
2023-03-02 16:41:17 -0300

    carrosel add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import Entradas from '../assets/entradas.png'
+import Prato from '../assets/pratos.png'
+import Sobremesas from '../assets/sobremesas.png'
 
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+     
+
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src={Prato}
+          alt="First slide"
+        />
+       
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src={Entradas}
+          alt="Second slide"
+        />
+       
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Sobremesas}
+          alt="Third slide"
+        />
+       
+      </Carousel.Item>
+    </Carousel>
+  );
+
+
     </div>
   )
 }
