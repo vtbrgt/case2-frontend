7ac305321531defb9938ff1bd5d2fc9247fc0666
Ismael Gonçalves da Silva <ismaelgj123567@gmail.com>
2023-03-02 16:56:56 -0300

    add login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+ import Button from 'react-bootstrap/Button';
+  import Form from 'react-bootstrap/Form';
+  
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+  const [email, setEmail] = userState("")
+
+    return (
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control type="email" placeholder="E-mail" />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+        <Button type="submit">
+          Entrar
+        </Button>
+      </Form>
+    );
+  }
 
 export default Login
--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate, BrowserRouter as Routers } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Sobre from '../pages/Sobre'
 import Menu from '../pages/Menu'
 import Login from '../pages/Login'
 import Contato from '../pages/Contato'
+import Admin from '../pages/admin'
 
 
 function Rotas() {
@@ -15,6 +16,10 @@ function Rotas() {
             <Route path="/menu" element={<Menu/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/contato" element={<Contato/>} />
+            <Route path="/contato" element={<Contato/>} />
+            
+            <Route path="/admin" element={<Admin/>} />
+
         </Routes>
     )
 }
