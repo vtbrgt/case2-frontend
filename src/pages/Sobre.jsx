5b49bab45bd7acb8be87b343df11e06da81e7dee
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2023-03-06 15:08:53 -0300

    criando página sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Api } from '../api/Api';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Loading from '../components/Loading';
+import Funcionario from '../components/Funcionarios';
+import './Sobre.css';
 
 const Sobre = () => {
+  const [funcionarios, setFuncionarios] = React.useState(null)
+  const [loading, setLoading] = React.useState(true)
+
+  async function getFuncionarios() {
+    try {
+      const {data} = await Api.get('/funcionarios');
+      setFuncionarios(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  React.useEffect(() => {
+    getFuncionarios()
+  })
+
   return (
-    <div>
-      <h1>Sobre</h1>
+    <div className='equipe'>
+      <h1>Nossa equipe</h1>
+      <CardGroup>
+      {funcionarios && funcionarios.map(funcionario => (
+        <Funcionario nome={funcionario.nome} sobrenome={funcionario.sobrenome} cargo={funcionario.cargo} key={funcionario.id}/>
+      ))}
+    </CardGroup>
+    {loading && <Loading/>}
     </div>
   )
 }
 
-export default Sobre
+export default Sobre;
