8339c4483e45f0cfb787e7eec0ebd30f04671133
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-03-08 22:28:54 -0300

    melhorias de estilo

--- src/components/Funcionarios.jsx
+++ src/components/Funcionarios.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-const Funcionario = ({nome, sobrenome, cargo}) => {
+const Funcionario = ({ nome, sobrenome, cargo }) => {
   return (
     <Row xs={'auto'} md={'auto'} className="m-2">
       <Col>
         <Card>
-        <Card.Body>
-          <Card.Title>{nome} {sobrenome}</Card.Title>
-          <Card.Text>{cargo}</Card.Text>
-        </Card.Body>
-      </Card>
+          <Card.Body>
+            <Card.Title>
+              {nome} {sobrenome}
+            </Card.Title>
+            <Card.Text>{cargo}</Card.Text>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
-      
-  )
-}
+  );
+};
 
-export default Funcionario
+export default Funcionario;
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 function Loading() {
   return (
-    <Spinner animation="border" role="status">
+    <Spinner animation="border" role="status" style={{ margin: '2rem 0' }}>
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
